4c0a9a3388ffdddc58d54c7de0bcf07ed47446da
aiden30015 <loolloo080313@gmail.com>
2025-07-16 21:11:14 +0900

    💄 :: Todo list 위젯 추가

---
+++ lib/todo_app/bloc/widgets/todo_list.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class TodoList extends StatelessWidget {
+
+  bool isCompleted;
+  final VoidCallback onChecked;
+  final String todo;
+  final String time;
+
+  TodoList({
+    required this.isCompleted,
+    required this.onChecked,
+    required this.todo,
+    required this.time,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: 300,
+      child: Row(
+        children: [
+          isCompleted ? 
+          IconButton(
+            onPressed: onChecked, 
+            icon: Icon(Icons.check_circle_outline),
+          ) :
+          IconButton(
+            onPressed: onChecked, 
+            icon: Icon(Icons.circle_outlined),
+          ),
+          Text(
+            todo,
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.black,
+              decoration: isCompleted ? TextDecoration.lineThrough : null,
+            ),
+          ),
+          Spacer(),
+          Text(
+            time,
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.grey
+            ), 
+          )
+        ],
+      ),
+    );
+  }
+}
