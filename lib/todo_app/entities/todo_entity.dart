384e798d2ccf3f0c1d75e49448d728f1c99a4a47
aiden30015 <loolloo080313@gmail.com>
2025-07-17 12:21:22 +0900

    ♻️ :: Todo 클래스를 엔티티로 수정

--- lib/todo_app/entities/todo_entity.dart
+++ lib/todo_app/entities/todo_entity.dart
@@ -8,4 +8,12 @@ class Todo {
     required this.isComplete,
     required this.todo,
   });
+
+  Todo copyWith({String? date, bool? isComplete, String? todo}) {
+    return Todo(
+      date: date ?? this.date,
+      isComplete: isComplete ?? this.isComplete,
+      todo: todo ?? this.todo,
+    );
+  }
 }
