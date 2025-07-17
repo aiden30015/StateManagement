a260bd0d99423fb342828352620c9b215acd3b25
aiden30015 <loolloo080313@gmail.com>
2025-07-17 09:34:43 +0900

    ✨ :: Getx 화면에 연결

---
+++ lib/todo_app/getx/screen/getx_todo_screen.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:state_management/todo_app/getx/getx/todo_getx.dart';
+import 'package:state_management/todo_app/widgets/add_todo_dialog.dart';
+import 'package:state_management/todo_app/widgets/todo_list.dart';
+
+class GetxTodoScreen extends StatelessWidget {
+  const GetxTodoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var now = DateTime.now();
+    String formatDate = DateFormat('M월 d일 EEEE').format(now);
+    final todoController = Get.put(TodoGetx());
+
+    String formatTimeOfDayToAmPm(TimeOfDay time) {
+      final now = DateTime.now();
+      final dt = DateTime(now.year, now.month, now.day, time.hour, time.minute);
+      final formatted = DateFormat('hh:mma').format(dt).toLowerCase(); 
+      return formatted;
+    }
+
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        formatDate,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 32,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        '${todoController.todoList.length}개의 할일',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                        Colors.blue,
+                      ),
+                      shape: MaterialStateProperty.all(CircleBorder()),
+                    ),
+                    onPressed: () async {
+                      final result = await showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AddTodoDialog();
+                        },
+                      );
+                      final selectTime = formatTimeOfDayToAmPm(result[1]);
+                      if (result != null) {
+                        todoController.addTodo(result[0], selectTime);
+                      }
+                    },
+                    child: Icon(Icons.add, color: Colors.white),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: todoController.todoList.length,
+                  itemBuilder: (context, index) {
+                    return Dismissible(
+                      key: ValueKey(todoController.todoList[index].todo),
+                      onDismissed: (direction) {
+                        todoController.DeleteTodo(index);
+                      },
+                      child: TodoList(
+                        isCompleted: todoController.todoList[index].isComplete,
+                        onChecked: () {
+                          todoController.CompleteTodo(index);
+                        },
+                        todo: todoController.todoList[index].todo,
+                        time: todoController.todoList[index].date,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
