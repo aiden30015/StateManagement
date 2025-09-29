5b93dbdde4d54e17a5c076a8aa842b7f6d853fe4
이주언 <loolloo080313@gmail.com>
2025-09-29 16:19:44 +0900

    💄 :: Todo get screen

--- lib/todo_app/getx/screen/getx_todo_screen.dart
+++ lib/todo_app/getx/screen/getx_todo_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
+import 'package:state_management/todo_app/core/constant/todo_base_scaffold.dart';
 import 'package:state_management/todo_app/getx/getx/todo_getx.dart';
 import 'package:state_management/todo_app/widgets/add_todo_dialog.dart';
+import 'package:state_management/todo_app/widgets/todo_appbar.dart';
 import 'package:state_management/todo_app/widgets/todo_list.dart';
 
 class GetxTodoScreen extends StatelessWidget {
@@ -12,98 +14,62 @@ class GetxTodoScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     var now = DateTime.now();
     String formatDate = DateFormat('M월 d일 EEEE').format(now);
-    final todoController = Get.put(TodoGetx());
+    final todoController = Get.find<TodoGetx>();
 
     String formatTimeOfDayToAmPm(TimeOfDay time) {
       final now = DateTime.now();
       final dt = DateTime(now.year, now.month, now.day, time.hour, time.minute);
-      final formatted = DateFormat('hh:mma').format(dt).toLowerCase(); 
+      final formatted = DateFormat('hh:mma').format(dt).toLowerCase();
       return formatted;
     }
 
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-          child: Obx( () =>Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        formatDate,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 32,
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      Text(
-                        '${todoController.todoList.length}개의 할일',
-                        style: TextStyle(
-                          color: Colors.blue,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
-                  ),
-                  ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(
-                        Colors.blue,
-                      ),
-                      shape: MaterialStateProperty.all(CircleBorder()),
-                    ),
-                    onPressed: () async {
-                      final result = await showDialog(
-                        context: context,
-                        builder: (context) {
-                          return AddTodoDialog();
-                        },
-                      );
-                      final selectTime = formatTimeOfDayToAmPm(result[1]);
-                      if (result != null) {
-                        todoController.addTodo(result[0], selectTime);
-                      }
-                    },
-                    child: Icon(Icons.add, color: Colors.white),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: todoController.todoList.length,
-                  itemBuilder: (context, index) {
-                    final selectTodo = todoController.todoList[index];
+    Future<void> addTodo() async {
+      final result = await showDialog(
+        context: context,
+        builder: (context) {
+          return AddTodoDialog();
+        },
+      );
+      final selectTime = formatTimeOfDayToAmPm(result[1]);
+      if (result != null) {
+        todoController.addTodo(result[0], selectTime);
+      }
+    }
 
-                    return Dismissible(
-                      key: ValueKey(selectTodo.isComplete),
-                      onDismissed: (direction) {
-                        todoController.DeleteTodo(index);
-                      },
-                      child: TodoList(
-                        isCompleted: selectTodo.isComplete,
-                        onChecked: () {
-                          todoController.CompleteTodo(index);
-                        },
-                        todo: selectTodo.todo,
-                        time: selectTodo.date,
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ],
-          ),
-          ),
+    return TodoBaseScaffold(
+      child: Obx(
+        () => Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TodoAppbar(
+              addTodo: addTodo,
+              todoLenght: todoController.todoList.length,
+              formatDate: formatDate,
+            ),
+            SizedBox(height: 20),
+            _buildTodoList(todoController),
+          ],
         ),
       ),
+    );
+  }
+
+  Widget _buildTodoList(TodoGetx todoController) {
+    return ListView.builder(
+      itemCount: todoController.todoList.length,
+      itemBuilder: (context, index) {
+        final todo = todoController.todoList[index];
+        return Dismissible(
+          key: ValueKey('${todo.todo}_$index'),
+          onDismissed: (_) => todoController.deleteTodo(index),
+          child: TodoList(
+            isCompleted: todo.isComplete,
+            onChecked: () => todoController.completeTodo(index),
+            todo: todo.todo,
+            time: todo.date,
+          ),
+        );
+      },
     );
   }
 }
