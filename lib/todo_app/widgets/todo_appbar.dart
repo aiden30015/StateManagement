dbe5d586521d7ce8183691a04eb0ff6b7493e314
이주언 <loolloo080313@gmail.com>
2025-09-29 15:33:20 +0900

    💄 :: Todo appbar

---
+++ lib/todo_app/widgets/todo_appbar.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class TodoAppbar extends StatelessWidget {
+  final VoidCallback addTodo;
+  final int todoLenght;
+  final String formatDate;
+
+  const TodoAppbar({
+    super.key,
+    required this.addTodo,
+    required this.todoLenght,
+    required this.formatDate,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              formatDate,
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 32,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              '${todoLenght}개의 할일',
+              style: TextStyle(color: Colors.blue, fontSize: 16),
+            ),
+          ],
+        ),
+        ElevatedButton(
+          style: ButtonStyle(
+            backgroundColor: WidgetStateProperty.all(Colors.blue),
+            shape: WidgetStateProperty.all(CircleBorder()),
+          ),
+          onPressed: () => addTodo(),
+          child: Icon(Icons.add, color: Colors.white),
+        ),
+      ],
+    );
+  }
+}
