08710b74cfeeef3b7f112f4d1c5337bf9c76b854
이주언 <loolloo080313@gmail.com>
2025-10-11 21:03:19 +0900

    ✨ :: RiverPod 사용해 todo 완성

--- lib/todo_app/riverpod/screen/riverpod_todo_screen.dart
+++ lib/todo_app/riverpod/screen/riverpod_todo_screen.dart
@@ -4,13 +4,14 @@ import 'package:intl/intl.dart';
 import 'package:state_management/todo_app/entities/todo_entity.dart';
 import 'package:state_management/todo_app/riverpod/provider/todo_provider.dart';
 import 'package:state_management/todo_app/widgets/add_todo_dialog.dart';
+import 'package:state_management/todo_app/widgets/todo_appbar.dart';
 import 'package:state_management/todo_app/widgets/todo_list.dart';
 
 class RiverpodTodoScreen extends ConsumerWidget {
   const RiverpodTodoScreen({super.key});
 
   @override
-  Widget build(BuildContext context,WidgetRef ref) {
+  Widget build(BuildContext context, WidgetRef ref) {
     var now = DateTime.now();
     String formatDate = DateFormat('M월 d일 EEEE').format(now);
     List<Todo> todo = ref.watch(todoProvider);
@@ -18,8 +19,21 @@ class RiverpodTodoScreen extends ConsumerWidget {
     String formatTimeOfDayToAmPm(TimeOfDay time) {
       final now = DateTime.now();
       final dt = DateTime(now.year, now.month, now.day, time.hour, time.minute);
-      final formatted = DateFormat('hh:mma').format(dt).toLowerCase(); 
+      final formatted = DateFormat('hh:mma').format(dt).toLowerCase();
       return formatted;
+    }
+
+    Future<void> addTodo() async {
+      final result = await showDialog(
+        context: context,
+        builder: (context) {
+          return AddTodoDialog();
+        },
+      );
+      final selectTime = formatTimeOfDayToAmPm(result[1]);
+      if (result != null) {
+        ref.read(todoProvider.notifier).addTodo(result[0], selectTime);
+      }
     }
 
     return Scaffold(
@@ -29,52 +43,10 @@ class RiverpodTodoScreen extends ConsumerWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        formatDate,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 32,
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      Text(
-                        '${todo.length}개의 할일',
-                        style: TextStyle(
-                          color: Colors.blue,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
-                  ),
-                  ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(
-                        Colors.blue,
-                      ),
-                      shape: MaterialStateProperty.all(CircleBorder()),
-                    ),
-                    onPressed: () async {
-                      final result = await showDialog(
-                        context: context,
-                        builder: (context) {
-                          return AddTodoDialog();
-                        },
-                      );
-                      final selectTime = formatTimeOfDayToAmPm(result[1]);
-                      if (result != null) {
-                        ref.read(todoProvider.notifier).addTodo(result[0], selectTime);
-                      }
-                    },
-                    child: Icon(Icons.add, color: Colors.white),
-                  ),
-                ],
+              TodoAppbar(
+                addTodo: addTodo,
+                todoLenght: todo.length,
+                formatDate: formatDate,
               ),
               SizedBox(height: 20),
               Expanded(
@@ -85,12 +57,16 @@ class RiverpodTodoScreen extends ConsumerWidget {
                     return Dismissible(
                       key: ValueKey(todoIndex.todo),
                       onDismissed: (direction) {
-                        ref.read(todoProvider.notifier).deleteTodo(todoIndex.date, todoIndex.todo);
+                        ref
+                            .read(todoProvider.notifier)
+                            .deleteTodo(todoIndex.date, todoIndex.todo);
                       },
                       child: TodoList(
                         isCompleted: todoIndex.isComplete,
                         onChecked: () {
-                          ref.read(todoProvider.notifier).isComplete(todoIndex.date, todoIndex.todo);
+                          ref
+                              .read(todoProvider.notifier)
+                              .isComplete(todoIndex.date, todoIndex.todo);
                         },
                         todo: todoIndex.todo,
                         time: todoIndex.date,
