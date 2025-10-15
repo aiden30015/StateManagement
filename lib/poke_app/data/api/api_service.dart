724ed6e0adfda9e65d24fd09d278c06aeeb7a9e1
이주언 <loolloo080313@gmail.com>
2025-10-15 11:48:25 +0900

    ✨ :: Poke api service 추가

---
+++ lib/poke_app/data/api/api_service.dart
@@ -0,0 +1,19 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:state_management/poke_app/data/dto/poke_item_response_dto.dart';
+
+part 'api_service.g.dart';
+
+@RestApi(baseUrl: "")
+abstract class ApiService {
+  factory ApiService(Dio dio, {String? baseUrl}) {
+    final envBaseUrl = dotenv.env['BASE_URL']!;
+    return _ApiService(dio, baseUrl: baseUrl ?? envBaseUrl);
+  }
+  @GET("/pokemon")
+  Future<PokeItemResponseDto> getItems(
+    @Query("limit") int limit,
+    @Query("offset") int offset,
+  );
+}
